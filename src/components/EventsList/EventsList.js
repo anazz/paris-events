9c8530a4cc0bc7d5cd9aee4478ce3999c7de1c17
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-15 01:56:03 +0200

    added the search form to the layout of the events list component

--- src/components/EventsList/EventsList.js
+++ src/components/EventsList/EventsList.js
@@ -8,6 +8,27 @@ const EventsList = (props) => {
     return (
         <div className="events-section-wrapper">
             <h2>Events</h2>
+
+            {/* SEARCH BY EVENT CATEGORY */}
+
+            <div className="search-wrapper">
+                    <form>
+                        <div className="search-form-wrapper">
+                        <input
+                            className="form-control"
+                            type="text"
+                            id="category-search"
+                            name="category"
+                            value=""
+                            onChange=""
+                        />
+                        </div>
+                        <button className="btn btn-info">Rechercher événement</button>
+                    </form>
+            </div>
+
+            {/* LISTED EVENTS */}
+
             <div className="events-list-wrapper">
                 <ul>
                     <Events />
