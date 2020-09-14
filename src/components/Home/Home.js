455271714e2203284c7e3d597f9c2a16a062e99a
Ana Zegheanu <ana.zegheanu@gmail.com>
2020-09-14 21:00:17 +0200

    added some style to the home and navbar components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Home.scss';
 
-const Home = () => {
-    return(
-        <h1>Paris Events</h1>
-    );    
+const Home = (props) => {
+    return (
+        <div className="home-wrapper">
+            <div className="title--wrapper">
+                <h1>Paris Events</h1>
+            </div>
+            <div className="description-wrapper">
+                <p>L'application qui permet de rechercher en direct les prochains événements Parisiens</p>
+            </div>
+            <div className="event-list-wrapper">
+                <ul>
+                    <li>
+                        <div className="event-wrapper">
+                            <img src="" alt=""/>
+                            <h2>Title: </h2>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )    
 };
 
 export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.scss';
 
 const Navbar = (props) => {
     return (
         <div className="navbar-wrapper">
-            <ul>
-                <li>
+            <img src="images/paper-plane.png" alt="logo paper plane image"/>
+                <ul>
+                    <li>
                     <Link to='/'>Accueil</Link>
-                </li>
-                <li>
-                    <Link to='/events'>Liste des événements</Link>
-                </li>
-                <li>
-                    <Link to='/favorites'>Favoris</Link>
-                </li>
-            </ul>
+                    </li>
+                    <li>
+                    <Link to='/events'>Liste des événements</Link>  
+                    </li>
+                    <li>
+                        <Link to='/favorites'>Favoris</Link>
+                    </li>
+                </ul>
         </div>
     )   
 };
